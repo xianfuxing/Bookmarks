554a6e1dfc90a594a437b61545025d1ad8efa23d
xianfuxing <xianfuxing@126.com>
2016-08-07 18:58:36 +0800

    # Add email authenticate

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -91,6 +91,11 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+)
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
